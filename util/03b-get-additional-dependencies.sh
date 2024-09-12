cd19f72b1f23d27e341c602648ee0d11c21bad16
Brendan Graetz <bguiz@users.noreply.github.com>
2024-09-12 12:18:40 +0800

    feat: run faster by front-loading first 'go build' of each task
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

--- util/03b-get-additional-dependencies.sh
+++ util/03b-get-additional-dependencies.sh
@@ -16,3 +16,15 @@ go mod download
 cd ${DIR}/../hcs
 go get ./...
 go mod download
+
+cd ${DIR}/../transfer
+echo "Build 'transfer'"
+time go build
+
+cd ${DIR}/../hts
+echo "Build 'hts'"
+time go build
+
+cd ${DIR}/../hcs
+echo "Build 'hcs'"
+time go build
